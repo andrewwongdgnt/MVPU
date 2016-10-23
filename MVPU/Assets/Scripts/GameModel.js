67c8ca5880c50cef4531b1005c7e7c29f73506b9
Andrew Wong <andrew.wong.dgnt@gmail.com>
2016-10-23 14:02:52 -0700

    first commit
    added some logic to game model

---
+++ MVPU/Assets/Scripts/GameModel.js
@@ -0,0 +1,123 @@
+﻿#pragma strict
+
+public class Cell
+{
+	private var _topBlocked : boolean;
+	private var _leftBlocked : boolean;
+	private var _bottomBlocked : boolean;
+	private var _rightBlocked : boolean;
+    
+    public function Cell(topBlocked : boolean, leftBlocked : boolean, bottomBlocked : boolean, rightBlocked : boolean)
+    {
+        _topBlocked = topBlocked;
+        _leftBlocked = leftBlocked;
+        _bottomBlocked = bottomBlocked;
+        _rightBlocked = rightBlocked;
+    }
+
+    public function isTopBlocked(): boolean
+    {
+    	return _topBlocked;
+    }
+
+    public function isLeftBlocked(): boolean
+    {
+    	return _leftBlocked;
+    }
+
+    public function isBottomBlocked(): boolean
+    {
+    	return _bottomBlocked;
+    }
+
+    public function isRightBlocked(): boolean
+    {
+    	return _rightBlocked;
+    }
+}
+
+
+public var grid = 
+[
+[new Cell(false,false,false,false)],
+[new Cell(false,false,false,false)]
+];
+
+public class Position
+{
+	private var _x : int;
+	private var _y : int;
+
+	public function Position(x : int, y : int)
+    {
+    	_x = x;
+    	_y = y;
+    }
+
+    public function getX() : int
+    {
+    	return _x;
+    }
+
+    public function setX(x : int) : void
+    {
+    	_x = x;
+    }
+
+    public function getY() : int
+    {
+    	return _y;
+    }
+
+    public function setY(y : int) : void
+    {
+    	_y = y;
+    }
+
+
+}
+public var playerPosition : Position;
+
+function Start ()
+{
+	
+}
+
+function Update ()
+{
+
+    if (Input.GetKeyDown(KeyCode.UpArrow))
+    {
+        if (playerPosition.getY()>0)
+        {
+        	playerPosition.setY(playerPosition.getY()-1);
+        }
+        Debug.Log(playerPosition.getX() + " , " + playerPosition.getY());
+	}
+	else if (Input.GetKeyDown(KeyCode.LeftArrow))
+    {
+        if (playerPosition.getX()>0)
+        {
+        	playerPosition.setX(playerPosition.getX()-1);
+        }
+        Debug.Log(playerPosition.getX() + " , " + playerPosition.getY());
+	}
+	else if (Input.GetKeyDown(KeyCode.DownArrow))
+    {
+        if (playerPosition.getY()<grid.length-1)
+        {
+        	playerPosition.setY(playerPosition.getY()+1);
+        }
+        Debug.Log(playerPosition.getX() + " , " + playerPosition.getY());
+	}
+	else if (Input.GetKeyDown(KeyCode.RightArrow))
+    {
+        if (playerPosition.getX()<grid[playerPosition.getX()].length-1)
+        {
+        	playerPosition.setX(playerPosition.getX()+1);
+        }
+        Debug.Log(playerPosition.getX() + " , " + playerPosition.getY());
+	}
+
+	//Debug.Log(grid[0][0].isTopBlocked());
+}
---
+++ MVPU/Assets/Scripts/Level1_1.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var gameModel : GameModel;
+
+function Start () {
+gameModel.grid = [
+[new Cell(true,false,false,false),new Cell(false,false,false,false),new Cell(false,false,false,false)],
+[new Cell(false,false,false,false),new Cell(false,false,false,false),new Cell(false,false,false,false)],
+[new Cell(false,false,false,false),new Cell(false,false,false,false),new Cell(false,false,false,false)]
+];
+gameModel.playerPosition = new Position(0,0);
+}
+
+function Update () {
+//Debug.Log("ww");
+}
