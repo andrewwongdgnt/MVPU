81e05891c23f66cdd1514aaf6549431b99c021a8
Andrew Wong <andrew.wong.dgnt@gmail.com>
2017-04-24 00:15:13 -0700

    d

--- MVPU/Assets/Levels/LevelManager.cs
+++ MVPU/Assets/Levels/LevelManager.cs
@@ -23,7 +23,7 @@ public class LevelManager
     }
 
     //meant to be mutable
-    public static LevelID levelToLoad = LevelID.TEST_LEVEL;
+    public static LevelID levelToLoad = LevelID.LEVEL_2_5;
 
     public static LevelID[][] WorldToLevelArr = new LevelID[][]
     {
